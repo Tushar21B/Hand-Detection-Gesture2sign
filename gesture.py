4c02309557cf601066119975d60901f6c8404168
Tushar21B <tusharblue666@gmail.com>
2024-10-28 20:37:01 +0530

    Add files via upload

---
+++ gesture.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+pip install opencv-python 
+
+
+# In[3]:
+
+
+pip install mediapipe
+
+
+# In[ ]:
+
+
+import cv2
+import mediapipe as mp
+
+#Hands module
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+# Initialize MediaPipe Drawing module for drawing landmarks
+mp_drawing = mp.solutions.drawing_utils
+
+# Open a video capture object (0 for the default camera)
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    # Flip the frame horizontally for a mirror effect
+    frame = cv2.flip(frame, 1)
+
+    # Convert the frame to RGB format
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame to detect hands
+    results = hands.process(frame_rgb)
+
+    # Check if hands are detected
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Draw landmarks on the frame
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # Display the frame with hand landmarks
+    cv2.imshow('Hand Movement Detection', frame)
+
+    # Exit when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
